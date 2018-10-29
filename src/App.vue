585b898b2c6b2a2ade0557bf6b6401026a06b31b
yangyang <yangyanjie@ochain.fun>
2018-10-29 10:13:11 +0800

    add-firsr

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <div class="view" v-if="loading">
+            <!-- <loading-layer></loading-layer> -->
+        </div>
+        <router-view class="view" :class="{hide:loading}"></router-view>
+    </div>
+</template>
+
+<script>
+    import {
+        mapGetters
+    } from 'vuex';
+
+    export default {
+        name: 'app',
+        computed: {
+            ...mapGetters({
+                loading: 'loading',
+            })
+        },
+        methods: {
+
+        },
+        
+    };
+</script>
+
+<style lang="scss">
+    @import './assets/css/reset.css';
+    #app {
+        width: 100%;
+        height: 100%;
+    }
+</style>
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="home">
+        <div class="header">hello, home!</div>
+        
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style lang="scss">
+ #home {
+     width: 100%;
+     height: 100%;
+     background: red;
+     .header {
+         width: 100%;
+         height: 100px;
+     }
+ }
+</style>
