bd03ce35d7d7cf46c7e31a2483b119b49996385a
yangyang <yangyanjie@ochain.fun>
2018-10-29 14:09:52 +0800

    add-js

--- build/dev-server.js
+++ build/dev-server.js
@@ -62,7 +62,8 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://localhost:' + port
+
+var uri = 'http://localhost:'+port+'/views/home.html'
 
 // console.log('> Starting dev server...')
 devMiddleware.waitUntilValid(() => {
---
+++ src/views/Login/login.js
@@ -0,0 +1,7 @@
+'use strict'
+import Vue from 'vue'
+// import Flex from '@/assets/js/flex'
+import App from './Login.vue'
+new Vue({
+    render: h => h(App)
+}).$mount('#app')
